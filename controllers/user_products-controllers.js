2f6a8956a3f3c7f436f18f84f9d42c1187adf2de
suchi ashok <suchiashok.97@gmail.com>
2024-06-11 00:32:38 -0400

    updated endpoints for user_products table

--- controllers/user_products-controllers.js
+++ controllers/user_products-controllers.js
@@ -35,7 +35,48 @@ const addUserProducts = async (req, res) => {
     res.status(500).send("An error occurred while adding user product");
   }
 };
+
+const updateUserProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { openDate, expirationMonths } = req.body;
+    if (!openDate || !expirationMonths) {
+      return res
+        .status(400)
+        .send("openDate and expirationMonths are required.");
+    }
+
+    await knex("user_products").where({ id }).update({
+      open_date: openDate,
+      expiration_months: expirationMonths,
+    });
+
+    res.status(200).send("User Product updated successfully");
+  } catch (error) {
+    console.log("Error updating user product", error);
+    res.status(500).send("An error occurred while updating user product");
+  }
+};
+
+const deleteUserProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await knex("user_products").where({ id }).del();
+
+    if(deleted) {
+      res.status(200).send(`User Product with id ${id} deleted successfully`)
+    } else {
+      res.status(404).send("User Product not found")
+    }
+  } catch (error) {
+    console.error("Error deleting user product:", error);
+    res.status(500).send("An error occurred while deleting user product");
+  }
+};
+
 module.exports = {
   getUserProducts,
   addUserProducts,
+  updateUserProduct,
+  deleteUserProduct,
 };
--- routes/user_products-routes.js
+++ routes/user_products-routes.js
@@ -7,4 +7,8 @@ router.route('/')
 .get(user_productsController.getUserProducts)
 .post(user_productsController.addUserProducts)
 
+router.route('/:id')
+.patch(user_productsController.updateUserProduct)
+.delete(user_productsController.deleteUserProduct);
+
 module.exports = router;
